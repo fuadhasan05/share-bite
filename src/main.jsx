d07ff4d4f9bc5623e21dd71016b64ec4be3fa264
Fuad Hasan <fuadhasan3503@gmail.com>
2025-07-09 11:28:10 +0600

    create privateroute

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import './index.css'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import AvailableFood from './pages/AvailableFood'
+import AddFood from './pages/AddFood'
+import ManageFoods from './pages/ManageFoods'
+import FoodRequest from './pages/FoodRequest'
 
 const router = createBrowserRouter([
   {
@@ -29,6 +32,18 @@ const router = createBrowserRouter([
       {
         path: 'signup',
         Component: SignUp,
+      },
+      {
+        path: 'addfood',
+        element: <AddFood></AddFood>,
+      },
+      {
+        path: 'manage-my-food',
+        element: <ManageFoods></ManageFoods>,
+      },
+      {
+        path: 'my-food-request',
+        element: <FoodRequest></FoodRequest>
       },
     ],
   },
