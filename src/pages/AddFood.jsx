fef7f157f2db36e175dc6db42a9f48ca376c4ae6
Fuad Hasan <fuadhasan3503@gmail.com>
2025-07-10 07:32:15 +0600

    update add food for Doner info

--- src/pages/AddFood.jsx
+++ src/pages/AddFood.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useContext } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../contexts/AuthProvider";
 
 const handleAddFood = (e) => {
   e.preventDefault();
@@ -8,20 +10,44 @@ const handleAddFood = (e) => {
   const newFood = Object.fromEntries(formData.entries());
 
   console.log(newFood);
+
+  // save coffee data
+  fetch(`${import.meta.env.VITE_API_URL}/add-food`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newFood),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      Swal.fire({
+        title: "Good Job!",
+        text: "Data Added Successfully",
+        icon: "success",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log(newFood);
 };
 
 const AddFood = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div className="px-24">
-      <div className="p-12 text-center space-y-4">
+    <div className="px-24 py-12">
+      <div className=" text-center space-y-4">
         <h1 className="text-6xl">Add Food</h1>
         <p>
           Please fill out the form below to add a new food donation. All fields
           are required unless marked optional.
         </p>
       </div>
-      <form onSubmit={handleAddFood}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <form className="py-12" onSubmit={handleAddFood}>
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-6">
           <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
             <label className="label">Food Name</label>
             <input
@@ -54,7 +80,7 @@ const AddFood = () => {
             />
           </fieldset>
           <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Expired Date/Time</label>
+            <label className="label">Expired Date</label>
             <input
               type="datetime-local"
               name="expiredDateTime"
@@ -83,17 +109,42 @@ const AddFood = () => {
         </fieldset>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-6 p-4">
           <label className="label">Food Status</label>
-          <input type="text" name="status" className="input w-full" />
+          <input
+            type="text"
+            name="status"
+            className="input w-full"
+            value="Available"
+            readOnly
+          />
+        </fieldset>
+        {/* Donor Section - prefilled and read-only */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-6 p-4 items-center gap-4">
+          <img
+            src={user?.photoURL}
+            alt="Donor"
+            className="rounded-full w-16 h-16"
+          />
+          <div>
+            <label className="label">Donor Name</label>
+            <input
+              type="text"
+              name="donorName"
+              className="input w-full mb-2"
+              value={user?.displayName || ""}
+              readOnly
+            />
+            <label className="label">Donor Email</label>
+            <input
+              type="email"
+              name="donorEmail"
+              className="input w-full"
+              value={user?.email || ""}
+              readOnly
+            />
+          </div>
         </fieldset>
         <input type="submit" className="btn w-full" value="Add Food" />
       </form>
-      <div className="mt-10 flex items-center gap-4 bg-base-200 p-6 rounded-box border border-base-300">
-        <img alt="Donor" className="rounded-full w-16 h-16" />
-        <div>
-          <div className="font-bold">Donor: </div>
-          <div className="text-sm text-gray-500"></div>
-        </div>
-      </div>
     </div>
   );
 };
