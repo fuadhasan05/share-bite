b648ebe191dd0c26b1b54b55e0ad163cd3fa290c
Fuad Hasan <fuadhasan3503@gmail.com>
2025-08-09 16:27:41 +0600

    create LoadingSpinner

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { use } from 'react'
 import { Navigate, useLocation } from 'react-router'
 import { AuthContext } from '../contexts/AuthProvider'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 
 const PrivateRoute = ({ children }) => {
@@ -9,7 +10,7 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation()
 
   if (loading) {
-    return <p>Loading...</p>
+    return <LoadingSpinner/>
   }
 
   if (user && user?.email) {
